eeb140522bcad74c9da8d74a559c2c5e1a6b46ae
RUSerious0o <d.o.konstantinov.0o@gmail.com>
2021-05-24 21:30:00 +0300

    Lesson 4 homework files added

---
+++ Lesson 4/data.h
@@ -0,0 +1,20 @@
+// 4.1
+int var_1 = 5;
+int var_2 = 16;
+int var_3 = 10;
+
+// 4.2
+int numsToCheck[] = { -5, 24, 17, 257 };
+
+// 4.3
+const int NUM_1 = 7;
+const int NUM_2 = 10;
+const int CONTROL_VALUE = 10;
+
+// 4.4
+const int MATRIX_SIZE = 5;
+int matrix[MATRIX_SIZE][MATRIX_SIZE] = {0};
+
+// 4.5
+int yearsToCheck[] = { 1900, 2020, 2019, -10, 1600 };
+
