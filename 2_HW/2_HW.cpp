128175e854a0802d24d6b714cb7354632c98dc3e
RUSerious0o <d.o.konstantinov.0o@gmail.com>
2021-11-25 14:46:40 +0300

    Adv CPP. Lesson 2 homework
    Classes: PointerSorter, TextAnalyser.

--- 2_HW/2_HW.cpp
+++ 2_HW/2_HW.cpp
@@ -8,31 +8,48 @@
 
 using namespace std;
 
-//template <class T>
-//void mySwap(T* const left, T* const right) {
-//    T* const temp = *left;
-//    left = right;
-//    right = temp;
-//}
-
+// 2.1
 template <class T>
 void mySwap(T& left, T& right) {
     T temp = right;    
-    *right = *left;
-    *left = *temp;
-}
-
-void simpleSwap(int*& left, int*& right) {
-    int* temp = right;
     right = left;
     left = temp;
 }
 
-void regularSwap(int& left, int& right) {
-    int temp = right;
-    right = left;
-    left = temp;
+// 2.2
+template <class T>
+void printVector(vector<T>& v) {
+    for (T item : v) cout << item << " ";
+    cout << endl;
 }
+
+template<class T>
+class PointerSorter {
+public:
+    PointerSorter(vector<T> &values) {
+        for (int i = 0; i < values.size(); i++) {
+            pointers.push_back(&values[i]);
+        }
+    }
+
+    void sortPointers() {
+        sort(pointers.begin(), pointers.end(), PointersComparator());
+    }
+
+    void printPointerValues() {
+        for (T* item : pointers) cout << *item << " ";
+        cout << endl;
+    }
+
+private:
+    vector<T*> pointers;
+
+    struct PointersComparator {
+        bool operator() (T*& left, T*& right) {
+            return *left < *right;
+        }
+    };
+};
 
 int main() {
     system("cls");
@@ -42,37 +59,34 @@ int main() {
 
     { // 2.1
         pair<string*, string*> stringsToSwap = { new string("First string"), new string("Second string") };
-        cout << stringsToSwap.first << " " << *stringsToSwap.first << endl;
-        cout << stringsToSwap.second << " " << *stringsToSwap.second << endl;
+        cout << &stringsToSwap.first << " " << stringsToSwap.first << " " << *stringsToSwap.first << endl;
+        cout << &stringsToSwap.second << " " <<stringsToSwap.second << " " << *stringsToSwap.second << endl;
 
         mySwap<string*>(stringsToSwap.first, stringsToSwap.second);
-        cout << stringsToSwap.first << " " << *stringsToSwap.first << endl;
-        cout << stringsToSwap.second << " " << *stringsToSwap.second << endl;
+        cout << &stringsToSwap.first << " " << stringsToSwap.first << " " << *stringsToSwap.first << endl;
+        cout << &stringsToSwap.second << " " << stringsToSwap.second << " " << *stringsToSwap.second << endl;
 
         delete stringsToSwap.first;
         delete stringsToSwap.second;
-
-        {
-            int* one = new int{ 15 };
-            int* two = new int{ 30 };
-            cout << one << " " << *one << " " << two << " " << *two << endl;
-            simpleSwap(one, two);
-            cout << one << " " << *one << " " << two << " " << *two << endl;
-        }
-
-        {
-            int one{ 15 };
-            int two{ 30 };
-            cout << one << " " << &one << " " << two << " " << &two << endl;
-            regularSwap(one, two);
-            cout << one << " " << &one << " " << two << " " << &two << endl;
-        }
+        cout << endl;
     }
 
     // 2.2
+    {
+        vector<string> values{ "Bob", "Mike", "Ann", "Alex", "John", "Sergey" };
+        PointerSorter<string> pointers(values);
+
+        printVector<string>(values);
+        pointers.printPointerValues();
+        cout << endl;
+
+        pointers.sortPointers();
+        printVector<string>(values);
+        pointers.printPointerValues();
+        cout << endl;
+    }
 
 
-    return 0;
     // 2.3
     TextAnalyser analyser;
     Timer timer;
